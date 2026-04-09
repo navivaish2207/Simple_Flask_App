ffc7e4ee5612d0281f12e9eaac4eb69446b0cd60
Vaishnavi P <navivaish2207@gmail.com>
2026-04-09 09:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+# Create app
 app = Flask(__name__)
 
+# Route for homepage
 @app.route('/')
 def hello():
     return "<h1>Hello from your Python Web App!</h1>"
 
+# Run server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
